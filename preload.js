979ab85f82879f5cd9fbb4dd6822497285697bb2
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2025-06-07 20:53:25 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,1 +1,6 @@
+const { contextBridge, ipcRenderer } = require('electron');
 
+contextBridge.exposeInMainWorld('butlerAPI', {
+    runButler: (folderPath) => ipcRenderer.invoke('run-butler', folderPath),
+    selectFolder: () => ipcRenderer.invoke('select-folder'),
+});
